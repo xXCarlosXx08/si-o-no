da41e2307f885b470119347c1e56877b2a454183
xXCarlosXx08 <130647173+xXCarlosXx08@users.noreply.github.com>
2023-04-27 10:35:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,18 +3,25 @@ input.onGesture(Gesture.Shake, function () {
     Número_aleatorio = randint(0, 7)
     if (Número_aleatorio == 1) {
         basic.showString("Si")
+        music.playMelody("- - C A E D - - ", 120)
     } else if (Número_aleatorio == 2) {
         basic.showString("No")
+        music.playMelody("E G E - G - A E ", 120)
     } else if (Número_aleatorio == 3) {
         basic.showString("A lo mejor")
+        music.playMelody("F E G - C C5 - B ", 120)
     } else if (Número_aleatorio == 4) {
         basic.showString("Probablemente")
+        music.playMelody("C5 E B F - - - - ", 120)
     } else if (Número_aleatorio == 5) {
         basic.showString("No creo")
+        music.playMelody("F D B F D E B - ", 120)
     } else if (Número_aleatorio == 6) {
         basic.showString("Tal vez")
+        music.playMelody("- E B F B - - - ", 120)
     } else {
         basic.showString(" No se")
+        music.playMelody("E G D F E A - - ", 120)
     }
 })
 let Número_aleatorio = 0
